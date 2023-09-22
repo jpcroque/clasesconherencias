6d91f751afea3eb86716099dfe97dd39a8717176
Jorge Calderon <jorgepablocalderonroque@gmail.com>
2023-09-22 16:11:44 -0400

    HTM - Formulario agragado, JS - clase persona agregada

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert("JS file test");
+class Person {
+    constructor(name, edad, profesion, cargo) {
+      this.name = name;
+      this.edad = edad;
+      this.profesion = profesion;
+      this.cargo = cargo;
+    }
+  
+    introduceSelf() {
+      console.log(`Nombre: ${this.name} Edad: ${this.edad} Profesion: ${this.profesion} cargo: ${this.cargo}`);
+    }
+  }
+
+  const gil = new Person("Gil", 25, "programador", "desarrollador");
+
+gil.introduceSelf(); // ¡Hola!, soy Gil
